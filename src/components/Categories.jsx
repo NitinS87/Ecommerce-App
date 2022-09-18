36f54a3d1091992636ce9fd6d4859dc61c3fa464
Nitin Sharma <nitins9868@gmail.com>
2022-09-18 14:48:21 +0000

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import CategoryItem from "../components/CategoryItem"
 import styled from "styled-components";
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
 
 const Container = styled.div`
   display: flex;
