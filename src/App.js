cab93a6521e2797bf02e4bd71d4fc442f97144e8
nitin <nitins9868@gmail.com>
2022-10-20 22:49:20 +0530

    Done Frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <Register/>
+      <Home />
+      {/* <Cart/> */}
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,12 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
   // background-color: black;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Wrapper = styled.div`
@@ -24,6 +26,7 @@ const Left = styled.div`
 const Language = styled.div`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
 const SearchContainer = styled.div`
@@ -36,6 +39,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -45,18 +49,21 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const MenuItems = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ flex: 2, fontSize: "12px", marginLeft: "10px" })}
 `;
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ justifyContent: "center" })}
 `;
 
 const NavBar = () => {
@@ -66,7 +73,7 @@ const NavBar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: 16 }} />{" "}
           </SearchContainer>
         </Left>
